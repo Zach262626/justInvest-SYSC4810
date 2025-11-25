1681bbd3f4286087f113f5f01cbb9941a2f2b459
Zach262626 <zachgallant26@gmail.com>
2025-11-24 19:30:33 -0500

    restart

--- src/main/java/mylabs/app/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package mylabs.app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-    Optional<Permission> findByName(String name);
-}
